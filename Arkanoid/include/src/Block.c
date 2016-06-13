982fd915d4c940947cd1f5edbb6f50a09dbbc7ac
Nicolai Weis <NicolaiWeis@adm-l3256.win.dtu.dk>
2016-06-13 11:00:48 +0200

    Block

---
+++ Arkanoid/include/src/Block.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUM_BLOCKS 50
+
+struct block {
+	int xleft, ytop, xright, ybot;
+};
+
+printBlocks(struct block b[NUM_BLOCKS], int x1, int y1, int color) {
+	int i, x = 196, y = 179, num = 0, x_temp = x1;
+
+	int a[6] = { 218, 192, 217, 191 };
+
+	while (num != NUM_BLOCKS) {
+		int y2 = y1 + 1;
+		while (x1 != 160) {
+
+			int x2 = x1 + 5;
+
+			for (i = y1; i < y2; i++) {
+				gotoxy(x1, i);
+				printf("%c", y);
+				gotoxy(x2, i);
+				printf("%c", y);
+			}
+			gotoxy(x1 + 1, y2);
+			for (i = x1 + 1; i < x2; i++) {
+				printf("%c", x);
+			}
+			gotoxy(x1 + 1, y1);
+			for (i = x1 + 1; i < x2; i++) {
+				printf("%c", x);
+			}
+
+			gotoxy(x1, y1);
+			printf("%c", a[0]);
+			gotoxy(x1, y2);
+			printf("%c", a[1]);
+			gotoxy(x2, y2);
+			printf("%c", a[2]);
+			gotoxy(x2, y1);
+			printf("%c", a[3]);
+
+			x1 += 7;
+			b[num].xleft = x1;
+			b[num].ytop = y1;
+			b[num].xright = x2;
+			b[num].ybot = y2;
+			num++;
+
+		}
+		y1 += 3;
+		x1 = x_temp;
+	}
+}
+void main() {
+	printBlocks(&b, 2, 2, 4);
+}
