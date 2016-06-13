c0c0f8c654b5a8238c71f305896cefc0fee822d8
Nicolai Weis <NicolaiWeis@adm-l3256.win.dtu.dk>
2016-06-13 11:25:24 +0200

    no message

---
+++ Arkanoid/include/Block.h
@@ -0,0 +1,15 @@
+//
+//  Block.h
+//  
+//
+//  Created by Nicolai Weis on 13/06/16.
+//
+//
+
+#ifndef Block_h
+#define Block_h
+
+printBlocks(struct block b[NUM_BLOCKS], int x1, int y1, int color);
+
+
+#endif /* Block_h */
