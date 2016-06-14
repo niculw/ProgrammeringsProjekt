9de54d0c70a5594dfb4e17b17ebaac0e8de00bfa
Nicolai <niculw@users.noreply.github.com>
2016-06-14 09:32:56 +0000

    Add files via upload

---
+++ Arkanoid/include/src/Life.c
@@ -0,0 +1,34 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+#include <eZ8.h>
+#include <sio.h>
+#include "ansi.h"#include "life.h"
+
+struct lives{
+int Lives;
+};
+
+
+void initLife(struct lives * L){
+(*L).Lives = 3;
+}
+
+void lifeChanger(struct lives * L , int collisionDetect, int Extra){
+	if (collisionDetect == -1){
+		(*L).Lives--;
+		}
+	else if (Extra == 1){
+		(*L).Lives++;
+	}
+
+	if ((*L).Lives == 0){
+
+	gotoxy(60,40);
+		printf("you lose");	   
+
+	}
+	gotoxy(200, 60);
+	printf("%d", (*L).Lives);
+
+}
+
