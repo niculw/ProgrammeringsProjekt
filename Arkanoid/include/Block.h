36ccb341a131bae5f4377a8f0e2f54ccd5fc18ee
Nicolai <niculw@users.noreply.github.com>
2016-06-14 13:27:22 +0000

    Add files via upload

--- Arkanoid/include/Block.h
+++ Arkanoid/include/Block.h
@@ -3,20 +3,25 @@
 //  
 //
 //  Created by Nicolai Weis on 13/06/16.
-//
+//																     void setLevelBlocks(struct block *b, int x1, int y1, int level, int num)
 //
 
 #ifndef Block_h
 #define Block_h
-#define NUM_BLOCKS
+#define NUM_BLOCKS 100
 
 struct block {
-    int xleft, ytop, xright;
+    int xleft, ytop, xright, life;
 };
+void setLife(struct block *b, int x1, int y1, int collision);
 
-void initBlocks(struct block b[NUM_BLOCKS], int x1, int y1, int x2, int num);
+void setLevelBlocks(struct block *b, int x1, int y1, int level, int num);
 
-void printBlocks(int x1, int y1);
+void setColor(struct block *b, int num);
+
+void printBlocks(struct block *b, int x1, int y1, int n);
+
+void initBlocks(struct block *b, int x1, int y1, int level, int length);
 
 
 #endif /* Block_h */
