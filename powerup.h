801f59e6798059352c28fa7f01fb025fe8f4364a
Nicolai <niculw@users.noreply.github.com>
2016-06-16 10:11:57 +0200

    Update powerup.h

--- powerup.h
+++ powerup.h
@@ -6,7 +6,7 @@ struct powerup{
 };
 
 void initPowerup(struct BallPos * ball,struct powerup * p );
-void drawPowerup(struct powerup * p);
+void drawPowerup(char select);
 void despawn((struct powerup * p);
 void powerup(int bh,struct powerup * p, struct lives * L, collisionDetect, int STRIKER_WIDTH, BallPos * ball);
 
